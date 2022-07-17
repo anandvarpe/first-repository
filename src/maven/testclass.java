726955c2589f577efaeb603ac82638bff753ce52
anand <anand@Happy>
2022-07-17 16:26:20 +0530

    [Anand] created test class

---
+++ src/maven/testclass.java
@@ -0,0 +1,12 @@
+package maven;
+
+public class testclass 
+{
+     public static void main(String[] args) {
+		
+    	 System.out.println("hii");
+    	 System.out.println("hii");
+    	 System.out.println("hii");
+	}
+
+}
